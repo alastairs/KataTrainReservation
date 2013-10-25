85008a7604f764dc89135f8dc1e386bf1a9129d6
Brice Fernandes <bfer@cbnl.com>
2013-10-25 11:57:20 +0100

    Refactor tests to introduce test data builders

--- python/ticket_office_test.py
+++ python/ticket_office_test.py
@@ -6,34 +6,60 @@ class TestBookingService(unittest.TestCase):
   def setUp(self):
     pass
 
-  def test_provides_reference(self):
+  def test_provides_booking_reference(self):
     assert "booking_reference" in BookingService().reserve("test_train", "4")
   
-  def test_provides_non_empty_reference_with_valid_reservation(self):
+  def test_provides_non_empty_booking_reference_with_valid_reservation(self):
     bs = BookingService()
-    train_data = {
-      "seats": { 
-        "1A" : {
-          "booking_reference": "",
-          "coach": "A",
-          "seat_number":"1"
-        }
-      }
-    }
+    train_data = TrainDataBuilder().with_seats(
+      { "1A" : SeatBuilder()
+                .with_coach("A")
+                .with_seat_number("1").build() }
+    ).build()
+    
     assert bs.reserve_on_train(train_data, 1)["booking_reference"]
 
-  def test_does_not_provide_reference_with_invalid_reservation(self):
+  def test_does_not_provide_booking_reference_with_invalid_reservation(self):
     bs = BookingService()
-    train_data = {
-      "seats": { 
-        "1A" : {
-          "booking_reference": "",
-          "coach": "A",
-          "seat_number":"1"
-        }
-      }
-    }
+    train_data = TrainDataBuilder().with_seats(
+      { "1A" : SeatBuilder()
+                .with_coach("A")
+                .with_seat_number("1").build() }
+    ).build();
+    
     assert not bs.reserve_on_train(train_data, 2)["booking_reference"]
+
+class SeatBuilder(object):
+  def __init__(self):
+    self.booking_reference = ""
+    self.coach = ""
+    self.seat_number = ""
+
+  def with_coach(self, new_coach):
+    self.coach = new_coach
+    return self
+
+  def with_seat_number(self, new_seat_number):
+    self.seat_number = new_seat_number
+    return self
+
+  def build(self):
+    return {
+      "booking_reference": self.booking_reference,
+      "coach": self.coach,
+      "seat_number": self.seat_number
+    }
+
+class TrainDataBuilder(object):
+ 
+  def with_seats(self, seats):
+    self.seats = seats
+    return self
+
+  def build(self):
+    return {
+      "seats": self.seats 
+    }
 
 if __name__ == '__main__':
     unittest.main()
